a253fcb9cf799ea6d6d90baee6be482bd35ea95b
thin-nd <thin-nd@trente.asia>
2021-04-13 00:03:02 +0700

    demo nhe anh

---
+++ .history/app/Http/Controllers/Controller_20210413000104.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use http\Env\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function saveData()
+    {
+        Post::create([
+            'username' => 'deo tin dc nhe anh em',
+            'email' => 'deo tin dc thang nao ca',
+            'lastName' => 'thank you'
+        ]);
+    }
+
+    public function getdata()
+    {
+        dd("ddfddfdf");
+    }
+}
---
+++ .history/database/seeds/UserSeeder_20201101230454.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = Faker::create();
+    	foreach (range(1,1000) as $index) {
+	        DB::table('users')->insert([
+                'username' => $faker->name,
+                'lastName' => $faker->lastName,
+                'email' => $faker->email,
+                'phone' => $faker->creditCardNumber,
+	            'password' => bcrypt('12345678'),
+	        ]);
+	}
+    }
+}
